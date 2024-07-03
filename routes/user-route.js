7afba6091edf3c2014336cb80e2130f8f1df5a3a
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-07-04 01:19:03 +0900

    refactor: 이미지 업로드 및 삭제로 인한 코드 수정 (회원가입, 개인정보수정 부분)

--- routes/user-route.js
+++ routes/user-route.js
@@ -1,5 +1,4 @@
 import express from "express";
-import multer from "multer";
 import * as userController from "../controllers/user-controller.js";
 import { userAuthenticate } from "../middleware/auth-middleware.js";
 
@@ -14,8 +13,8 @@ router.post("/auth/kakao", userController.kakaoLogin); // 진행 중
 // api 개발
 router.get("/getAllUser", userController.getAllUser); // 전체 유저 조회
 router.get("/auth/duplicatedEmail/:email", userController.duplicatedEmail); // 중복 이메일 체크
-router.post("/auth/register", multer().none(), userController.register); // 회원가입
-router.put("/user/updateUser", multer().none(), userController.updateUser); // 개인정보 수정
+router.post("/auth/register", userController.register); // 회원가입
+router.put("/user/updateUser", userController.updateUser); // 개인정보 수정
 router.post("/user/withdraw", userController.withdraw); // 유저 탈퇴
 router.patch("/user/language", userController.language); // 언어 변경
 
