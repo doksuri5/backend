64b121e4e0f59ae92fbd85ce896d9e0324d6f2cc
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-07-05 02:52:28 +0900

    refactor: 주식 스키마 컬럼명 변경

--- schemas/stock-schema.js
+++ schemas/stock-schema.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 
 const StockSchema = new mongoose.Schema({
-  icon: { type: String, required: true },
-  stock_name: { type: String, required: true, unique: true },
-  symbol_code: { type: String, required: true },
-  reuters_code: { type: String, required: true },
+  stockName: { type: String, required: true, unique: true },
+  symbolCode: { type: String, required: true },
+  reutersCode: { type: String, required: true },
   price: { type: Number, required: true },
   nationType: { type: String, required: true },
   compareToPreviousClosePrice: { type: Number, required: true },
   fluctuationsRatio: { type: Number, required: true },
+  market_price: { type: Number, default: 0 },
   investment_index: { type: Number, default: 0 },
   profitability: { type: Number, default: 0 },
   growth_rate: { type: Number, default: 0 },
