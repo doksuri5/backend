69cb7e00b83b65db2bf5071e68631c6c2eae06c7
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-07-05 16:42:22 +0900

    feat: 인증 코드 메일 발송 및 검증 API

--- routes/user-route.js
+++ routes/user-route.js
@@ -13,6 +13,8 @@ router.post("/auth/kakao", userController.kakaoLogin); // 진행 중
 // api 개발
 router.get("/getAllUser", userController.getAllUser); // 전체 유저 조회
 router.get("/auth/duplicatedEmail/:email", userController.duplicatedEmail); // 중복 이메일 체크
+router.post("/auth/sendEmail", userController.sendEmail); // 인증코드 이메일 발송
+router.post("/auth/verifyCode", userController.verifyCode); // 인증코드 검증
 router.post("/auth/register", userController.register); // 회원가입
 router.put("/user/updateUser", userController.updateUser); // 개인정보 수정
 router.post("/user/withdraw", userController.withdraw); // 유저 탈퇴
