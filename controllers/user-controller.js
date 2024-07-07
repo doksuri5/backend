f5e9dc27f41101bd3d39aaab8dafaf1f9206fcbe
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-07-07 17:07:19 +0900

    refactor: 사용하지 않는 코드 삭제와 탈퇴 여부 체크 및 change _id to sns_id

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -14,17 +14,7 @@ import deleteFileFromS3 from "../middleware/imageDelete.js";
 import { send_main_func } from "../utils/emailSendUtil.js";
 import { getKoreanTime } from "../utils/getKoreanTime.js";
 
-export const validation = async (req, res) => {
-  const user = await User.findById(req.session.userId);
-
-  if (!user) {
-    res.status(401).json({ ok: false, message: "로그인 해주시기 바랍니다." });
-  } else {
-    res.status(200).json({ ok: true, message: "ok" });
-  }
-};
-
-// 전체 유저 조회 (테스트)
+// 전체 유저 조회 (테스트 용도)
 export const getAllUser = async (req, res) => {
   try {
     const user = await User.find();
@@ -42,208 +32,11 @@ export const getAllUser = async (req, res) => {
   }
 };
 
-// 로그인 (JWT 생성)
-export const login = async (req, res) => {
-  const { login_email, login_pw } = req.body;
-  try {
-    await connectDB();
-    console.log(login_email, login_pw);
-
-    const user = await User.findOne({ email: login_email });
-
-    // 존재하지 않는 이메일인 경우
-    if (!user) {
-      res.status(404).json({
-        ok: false,
-        message: "존재하지 않는 이메일입니다.",
-      });
-      return;
-    }
-
-    // 비밀번호 해시 검증
-    const isMatch = await user.comparePassword(login_pw);
-
-    // 비밀번호가 일치하지 않을 때
-    if (!isMatch) {
-      res.status(401).json({
-        ok: false,
-        message: "비밀번호가 일치하지 않습니다.",
-      });
-      return;
-    }
-
-    const userInfo = {
-      user_id: user._id,
-      user_email: user.email,
-      user_name: user.name,
-      user_language: user.language,
-    };
-
-    req.session.userId = user._id; // 세션 저장
-
-    // // JWT access Token 생성
-    // const accessToken = jwt.generateAccessToken(userInfo);
-
-    // // JWT refresh Token 생성
-    // const refreshToken = jwt.generateRefreshToken(userInfo);
-
-    // // JWT 쿠키에 저장
-    // res.cookie("accessToken", accessToken, {
-    //   domain: process.env.COOKIE_DOMAIN,
-    //   path: "/",
-    //   httpOnly: true,
-    //   sameSite: process.env.COOKIE_SAMESITE,
-    //   secure: process.env.COOKIE_SECURE,
-    // });
-    // res.cookie("refreshToken", refreshToken, {
-    //   domain: process.env.COOKIE_DOMAIN,
-    //   path: "/",
-    //   httpOnly: true,
-    //   sameSite: process.env.COOKIE_SAMESITE,
-    //   secure: process.env.COOKIE_SECURE,
-    // });
-
-    // 로그 추가
-    await Log.create({ user_email: user.email });
-
-    res.status(200).json({
-      ok: true,
-      data: {
-        user_id: user._id,
-        user_email: user.email,
-        user_name: user.name,
-        user_language: user.language,
-      },
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({
-      ok: false,
-      message: err.message,
-    });
-    return;
-  }
-};
-
-export const naverLogin = async (req, res, next) => {
-  const { code, state } = req.body;
-
-  try {
-    const {
-      data: { access_token, expires_in, refresh_token },
-    } = await axios.post(
-      `https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=${process.env.NAVER_CLIENT_ID}&client_secret=${process.env.NAVER_CLIENT_SECRET}&redirect_uri=${process.env.NAVER_REDIRECT_URL}&code=${code}&state=${state}`,
-
-      {
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
-        },
-      }
-    );
-
-    const {
-      data: { response },
-    } = await axios.post(
-      "https://openapi.naver.com/v1/nid/me",
-      {},
-      {
-        headers: {
-          Authorization: `Bearer ${access_token}`,
-          "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
-        },
-      }
-    );
-
-    const exUser = await User.findOne({
-      email: response.email,
-      login_type: "naver",
-    });
-
-    // 로그인해서 회원가입 한 적이 있으면
-    req.session.auth = {
-      access_token,
-      expires_in,
-      refresh_token,
-    };
-    if (exUser) {
-      req.session.userId = exUser._id;
-      res.status(200).json({ ok: true, message: "로그인 진행" });
-    }
-    // 회원가입을 해야 되는 경우
-    else {
-      res.status(200).json({ ok: true, message: "회원가입 진행", data: response });
-    }
-  } catch (error) {
-    console.error(error);
-    return next(error);
-  }
-};
-
-// 카카오 로그인 (해야 함)
-export const kakaoLogin = async (req, res, next) => {
-  const { code } = req.body;
-
-  try {
-    const {
-      data: { access_token, expires_in, refresh_token, refresh_token_expires_in },
-    } = await axios.post(
-      `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_REDIRECT_URL}&code=${code}`,
-
-      {
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
-        },
-      }
-    );
-
-    const { data } = await axios.post(
-      "https://kapi.kakao.com/v2/user/me",
-      {},
-      {
-        headers: {
-          Authorization: `Bearer ${access_token}`,
-          "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
-        },
-      }
-    );
-
-    const exUser = await User.findOne({
-      snsId: data.id,
-      provider: "kakao",
-    });
-
-    if (exUser) {
-      req.session.userId = exUser._id;
-    } else {
-      const newUser = await User.create({
-        email: data?.properties?.email,
-        nick: data?.properties?.nickname,
-        snsId: data.id,
-        provider: "kakao",
-      });
-
-      req.session.userId = newUser._id;
-    }
-
-    req.session.auth = {
-      access_token,
-      expires_in,
-      refresh_token,
-      refresh_token_expires_in,
-    };
-
-    res.status(200).json({ message: "카카오 로그인 성공" });
-  } catch (error) {
-    console.error(error);
-    return next(error);
-  }
-};
-
-// 언어 변경
+// 언어 변경 (프론트로 변경)
 export const language = async (req, res) => {
   try {
     // 유저 쿠키 값을 가지고 user_id(인덱스) 값 가져오기
-    const userId = "6682e16dfae77a1869b25865";
+    const sns_id = "6682e16dfae77a1869b25865";
 
     const { language } = req.body;
 
@@ -259,14 +52,14 @@ export const language = async (req, res) => {
       return;
     });
 
-    const user = await User.findOneAndUpdate({ _id: userId }, { $set: { language: language } }, { new: true });
+    const user = await User.findOneAndUpdate({ sns_id }, { $set: { language: language } }, { new: true });
     res.status(200).json({ ok: true, data: user });
   } catch (err) {
     res.status(500).json({ ok: false, message: err.message });
   }
 };
 
-// 아이디(이메일) 중복 확인
+// 아이디(이메일) 중복 확인 (프론트로 변경)
 export const duplicatedEmail = async (req, res) => {
   try {
     const { email } = req.params;
@@ -283,7 +76,8 @@ export const duplicatedEmail = async (req, res) => {
       return;
     });
 
-    const user = await User.findOne({ email: email }).exec();
+    // (탈퇴여부 체크)
+    const user = await User.findOne({ email: email, is_delete: false }).exec();
     if (user) {
       res.status(200).json({ ok: true, data: user.email });
     } else {
@@ -297,8 +91,8 @@ export const duplicatedEmail = async (req, res) => {
 // 유저 탈퇴
 export const withdraw = async (req, res) => {
   try {
-    // 유저 쿠키 값을 가지고 user_id(인덱스) 값 가져오기
-    const userId = "6682e16dfae77a1869b25865";
+    // 유저 쿠키 값을 가지고 sns_id 값 가져오기
+    const sns_id = "6682e16dfae77a1869b25865";
 
     const { email, reason, reason_other = null } = req.body;
 
@@ -308,24 +102,31 @@ export const withdraw = async (req, res) => {
       return;
     });
 
-    const user = await User.findOneAndUpdate(
-      { _id: userId },
-      { $set: { is_delete: true, deleted_at: getKoreanTime() } },
-      { new: true }
-    );
+    const user = await User.findOne({ sns_id, is_delete: false });
 
     // 클라이언트에서 보낸 이메일과 세션으로 DB 조회한 이메일이 다른 경우
-    if (email !== user.email) {
+    if (!user || email !== user.email) {
       res.status(400).json({ ok: false, message: "입력한 이메일이 현재 사용자의 이메일과 일치하지 않습니다." });
       return;
     }
 
+    const updatedUser = await User.findOneAndUpdate(
+      { sns_id, email, is_delete: false },
+      { $set: { is_delete: true, deleted_at: getKoreanTime() } },
+      { new: true }
+    );
+
     await Withdraw.create({
-      user_id: user._id,
-      user_email: user.email,
-      reason: reason,
-      reason_other: reason_other,
+      user_snsId: updatedUser.sns_id,
+      user_email: updatedUser.email,
+      reason,
+      reason_other,
     });
+
+    await InterestStock.findOneAndUpdate(
+      { user_snsId: updatedUser.sns_id, is_delete: false },
+      { $set: { is_delete: true } }
+    );
 
     res.status(200).json({ ok: true, message: "탈퇴 완료" });
   } catch (err) {
@@ -346,8 +147,8 @@ export const register = [
         return;
       });
 
-      // 동일한 이메일을 가진 사용자가 이미 존재하는지 확인
-      const existingUser = await User.findOne({ email });
+      // 동일한 이메일을 가진 사용자가 이미 존재하는지 확인 (탈퇴여부 체크)
+      const existingUser = await User.findOne({ email, is_delete: false });
       if (existingUser) {
         res.status(400).json({ ok: false, message: "이미 회원가입이 된 이메일입니다." });
         return;
@@ -416,8 +217,8 @@ export const registerSocial = [
         return;
       });
 
-      // 동일한 이메일을 가진 사용자가 이미 존재하는지 확인
-      const existingUser = await User.findOne({ email });
+      // 동일한 이메일을 가진 사용자가 이미 존재하는지 확인 (탈퇴여부 체크)
+      const existingUser = await User.findOne({ email, is_delete: false });
       if (existingUser) {
         res.status(400).json({ ok: false, message: "이미 회원가입이 된 이메일입니다." });
         return;
@@ -463,8 +264,8 @@ export const updateUserProfile = [
   uploadProfileImg.single("profile"),
   async (req, res) => {
     try {
-      // 유저 쿠키 값을 가지고 user_id(인덱스) 값 가져오기
-      const userId = "66851b104c108bdb12c7973e";
+      // 유저 쿠키 값을 가지고 sns_id 값 가져오기
+      const sns_id = "66851b104c108bdb12c7973e";
 
       const { nickname, gender, interest_stocks } = req.body;
 
@@ -475,7 +276,7 @@ export const updateUserProfile = [
       });
 
       // 유저 조회
-      const user = await User.findById(userId);
+      const user = await User.findOne({ sns_id, is_delete: false });
       if (!user) {
         res.status(404).json({ ok: false, message: "사용자를 찾을 수 없습니다." });
         return;
@@ -501,16 +302,15 @@ export const updateUserProfile = [
       }
 
       // 프로필 수정
-      await User.findByIdAndUpdate(userId, {
-        $set: {
-          nickname,
-          gender,
-          profile: new_profile_img,
-        },
-      });
+      await User.findOneAndUpdate(
+        { sns_id, is_delete: false },
+        {
+          $set: { nickname, gender, profile: new_profile_img },
+        }
+      );
 
       // 유저 관심주식 조회
-      const interestStock = await InterestStock.findOne({ user_email: user.email });
+      const interestStock = await InterestStock.findOne({ user_snsId: user.sns_id, is_delete: false });
 
       // body에 관심 주식을 넣은 경우
       if (interest_stocks && interest_stocks.length > 0) {
@@ -541,7 +341,7 @@ export const updateUserInfo = async (req, res) => {
     const { email, password, phone, birth } = req.body;
 
     // 유저 쿠키 값을 가지고 user_id(인덱스) 값 가져오기
-    const userId = "66851b104c108bdb12c7973e";
+    const sns_id = "66851b104c108bdb12c7973e";
 
     // 데이터베이스 연결
     await connectDB().catch((err) => {
@@ -550,7 +350,7 @@ export const updateUserInfo = async (req, res) => {
     });
 
     // 고정 값으로 보낸 이메일과 DB에 저장된 이메일이 같은지 확인
-    const user = await User.findById(userId).select("+password");
+    const user = await User.findOne({ sns_id, is_delet: false }).select("+password");
     if (!user) {
       res.status(404).json({ ok: false, message: "사용자를 찾을 수 없습니다." });
       return;
@@ -569,13 +369,16 @@ export const updateUserInfo = async (req, res) => {
     }
 
     // 프로필 수정
-    await User.findByIdAndUpdate(userId, {
-      $set: {
-        password: hashedPassword,
-        phone,
-        birth,
-      },
-    });
+    await User.findOneAndUpdate(
+      { sns_id, is_delete: false },
+      {
+        $set: {
+          password: hashedPassword,
+          phone,
+          birth,
+        },
+      }
+    );
 
     res.status(200).json({ ok: true, message: "회원 정보 수정 완료" });
   } catch (err) {
@@ -583,11 +386,13 @@ export const updateUserInfo = async (req, res) => {
   }
 };
 
-// 인증코드 이메일 발송
+// 인증코드 이메일 발송 (탈퇴여부 체크)
 export const sendEmail = async (req, res) => {
   const { email } = req.body;
   try {
     await connectDB();
+
+    // (탈퇴여부 체크)
     const user = await User.findOne({ email });
     if (user) {
       res.status(401).json({ ok: false, message: "이미 가입된 이메일입니다." });
