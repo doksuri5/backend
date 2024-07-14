c9ff0ebc2b8e7569d70f3c0d6852e1b01d461eff
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-07-14 21:58:55 +0900

    refactor: 뉴스 스키마 수정

--- schemas/news-schema.js
+++ schemas/news-schema.js
@@ -2,13 +2,12 @@ import mongoose from "mongoose";
 
 const NewsSchema = new mongoose.Schema({
   index: { type: String, required: true },
-  stock_name: { type: String, required: true },
   publisher: {
     ko: { type: String, required: true },
     en: { type: String, required: true },
     ch: { type: String, required: true },
     jp: { type: String, required: true },
-    fr: { type: String, required: true },
+    // fr: { type: String, required: true },
   },
   thumbnail_url: { type: String, required: true },
   title: {
@@ -16,35 +15,35 @@ const NewsSchema = new mongoose.Schema({
     en: { type: String, required: true },
     ch: { type: String, required: true },
     jp: { type: String, required: true },
-    fr: { type: String, required: true },
+    // fr: { type: String, required: true },
   },
   description: {
     ko: { type: String, required: true },
     en: { type: String, required: true },
     ch: { type: String, required: true },
     jp: { type: String, required: true },
-    fr: { type: String, required: true },
+    // fr: { type: String, required: true },
   },
   published_time: { type: String, required: true },
   link: { type: String, required: true },
   content: {
-    ko: { type: String },
-    en: { type: String },
-    ch: { type: String },
-    jp: { type: String },
-    fr: { type: String },
+    ko: { type: String, required: true },
+    en: { type: String, required: true },
+    ch: { type: String, required: true },
+    jp: { type: String, required: true },
+    // fr: { type: String, required: true },
   },
   content_img: { type: String, required: true },
   relative_stock: { type: [String] },
-  count: { type: Number, default: 0 },
+  score: { type: Number, default: 0 },
+  view: { type: Number, default: 0 },
   ai_summary: {
     ko: { type: String },
     en: { type: String },
     ch: { type: String },
     jp: { type: String },
-    fr: { type: String },
+    // fr: { type: String },
   },
-  view: { type: Number, default: 0 },
 });
 
 const News = mongoose.models.News || mongoose.model("News", NewsSchema);
