85f8bf5e4a38a986466b510c63fb0d8fbfd4075c
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-07-15 13:32:18 +0900

    refactor: 관심종목 뉴스 API 유저 로그인 체크 확인

--- routes/news-route.js
+++ routes/news-route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import * as newsController from "../controllers/news-controller.js";
+import { checkLoggedIn } from "../middleware/auth-middleware.js";
 
 const router = express.Router();
 
 router.get("/news/getTodayPopularNews", newsController.getTodayPopularNews); // 오늘 인기있는 뉴스
-router.get("/news/getInterestStockNews", newsController.getInterestStockNews); // 관심종목과 관련된 뉴스
+router.get("/news/getInterestStockNews", checkLoggedIn, newsController.getInterestStockNews); // 관심종목과 관련된 뉴스
 router.get("/news/getRecentNews", newsController.getRecentNews); // 최신 뉴스
 router.get("/news/getNews/:index", newsController.getNews); // 1개 뉴스 조회 (관련 기사 포함)
 router.get("/news/hotNews", newsController.hotNews); // 주요 뉴스
