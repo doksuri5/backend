583581a3596749f078d6cb506220e4b0f04b4652
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-07-16 02:17:48 +0900

    add: STOCK_REUTERS_CODE 상수 추가

--- constants/app.constants.js
+++ constants/app.constants.js
@@ -1,8 +1,9 @@
-export const STOCK_NAME_LIST = [
-  "애플",
-  "마이크로소프트",
-  "테슬라",
-  "아마존닷컴",
-  "알파벳 Class A",
-  "유니티소프트웨어",
-];
+export const STOCK_NAME_LIST = ["애플", "마이크로소프트", "테슬라", "아마존닷컴", "알파벳 Class A", "유니티소프트웨어"];
+export const STOCK_REUTERS_CODE = {
+  애플: "APPL.O",
+  마이크로소프트: "MSFT.O",
+  테슬라: "TSLA.O",
+  아마존닷컴: "AMZN.O",
+  "알파벳 Class A": "GOOGL.O",
+  유니티: "U",
+};
