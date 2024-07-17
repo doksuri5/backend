ef6eb817bd327be058ef5a52c8e422c6b2b8aebb
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-07-17 18:00:42 +0900

    feat: 최근 검색어 delete 라우터 생성

--- routes/recentSearch-route.js
+++ routes/recentSearch-route.js
@@ -9,6 +9,7 @@ router.get("/getPopularSearches", recentSearchController.getPopularSearches); //
 router.get("/getRecentSearches", checkLoggedIn, recentSearchController.getRecentSearches); // 발견 페이지 - 최근 검색어 보여줄 때 사용
 router.get("/getRecentSearchDetails", checkLoggedIn, recentSearchController.getRecentSearchDetails); // 관심 종목 페이지 모달 - 최근 검색한 종목 보여줄 때 사용
 router.patch("/saveRecentSearch", checkLoggedIn, recentSearchController.saveRecentSearch); // 최근 검색어 저장
-router.delete("/deleteRecentSearches", checkLoggedIn, recentSearchController.deleteRecentSearches); // 유저 인덱스 따른 최근 검색어 삭제
+router.delete("/deleteRecentSearches", checkLoggedIn, recentSearchController.deleteRecentSearches); // 유저 인덱스 따른 최근 검색어 및 검색한 주식 전부 삭제
+router.delete("/deleteRecentSearchesItem", checkLoggedIn, recentSearchController.deleteRecentSearchesItem); // 유저 인덱스 따른 최근 검색어 개별 삭제
 
 export default router;
