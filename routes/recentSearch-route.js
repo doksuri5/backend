6bf8d94ea00c548cb0d3830b329863727822029e
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-07-17 16:10:09 +0900

    refactor: route 수정

--- routes/recentSearch-route.js
+++ routes/recentSearch-route.js
@@ -4,10 +4,11 @@ import { checkLoggedIn } from "../middleware/auth-middleware.js";
 
 const router = express.Router();
 
-router.get("/getRecentSearches", recentSearchController.getRecentSearches); // 발견 페이지 - 최근 검색어 보여줄 때 사용
-router.get("/getRecentSearchDetails", recentSearchController.getRecentSearchDetails); // 관심 종목 페이지 모달 - 최근 검색한 종목 보여줄 때 사용
+router.get("/getPopularSearchesName", recentSearchController.getPopularSearchesName); // 인기 검색어 가져오기 이름만
+router.get("/getPopularSearches", recentSearchController.getPopularSearches); // 인기 검색어 가져오기 디테일 정보까지
+router.get("/getRecentSearches", checkLoggedIn, recentSearchController.getRecentSearches); // 발견 페이지 - 최근 검색어 보여줄 때 사용
+router.get("/getRecentSearchDetails", checkLoggedIn, recentSearchController.getRecentSearchDetails); // 관심 종목 페이지 모달 - 최근 검색한 종목 보여줄 때 사용
 router.patch("/saveRecentSearch", checkLoggedIn, recentSearchController.saveRecentSearch); // 최근 검색어 저장
-router.delete("/deleteRecentSearches", recentSearchController.deleteRecentSearches); // 유저 인덱스 따른 최근 검색어 삭제
-router.get("/getPopularSearches", recentSearchController.getPopularSearches); // 인기 검색어 가져오기
+router.delete("/deleteRecentSearches", checkLoggedIn, recentSearchController.deleteRecentSearches); // 유저 인덱스 따른 최근 검색어 삭제
 
 export default router;
