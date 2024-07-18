04a1306e741d2936f28faf6cf3bac859159ec2b8
윤태현 <yoonth0919@gmail.com>
2024-07-18 21:55:31 +0900

    refactor: 이름 변경 및 검색 API 기능 진행

--- controllers/search-controller.js
+++ controllers/search-controller.js
@@ -23,14 +23,54 @@ export const getRecentSearches = async (req, res) => {
     // 데이터베이스 연결
     await connectDB();
 
+    // 유저 정보 조회
+    const user = await User.findOne({ sns_id: snsId, is_delete: false });
+    if (!user) {
+      res.status(401).json({ ok: false, message: "존재하지 않는 유저입니다." });
+      return;
+    }
+
     const searches = await RecentSearchText.find(
-      { user_snsId: snsId },
+      { user_snsId: snsId, is_delete: false },
       { _id: 0, search_text: 1, search_date: 1 }
     ).sort({
       search_date: -1,
     });
 
     res.status(200).json({ ok: true, data: searches });
+  } catch (err) {
+    res.status(500).json({ ok: false, message: err.message });
+  }
+};
+
+export const getSearchStocks = async (req, res) => {
+  try {
+    const { search_text } = req.params;
+    const { snsId } = req.session;
+
+    // 검색어가 6가지 종목 안에 포함되는 경우 (초성 포함)
+    const searchStockList = Object.entries(VARIOUS_STOCK_TO_NAME)
+      .filter((stock) => hangulIncludes(stock[0], search_text))
+      .map((stock) => stock[1]);
+
+    if (searchStockList.length === 0) {
+      res.status(200).json({ ok: true, data: [] });
+      return;
+    }
+
+    // 데이터베이스 연결
+    await connectDB();
+
+    // 유저 정보 조회
+    const user = await User.findOne({ sns_id: snsId, is_delete: false });
+    if (!user) {
+      res.status(401).json({ ok: false, message: "존재하지 않는 유저입니다." });
+      return;
+    }
+
+    const stockData = await Stock.find({ stock_name: { $in: searchStockList } }); // stockNames 리스트에 있는 값들만 조회
+
+    res.status(200).json({ ok: true, data: stockData });
   } catch (err) {
     res.status(500).json({ ok: false, message: err.message });
   }
@@ -45,7 +85,7 @@ export const getRecentSearchDetails = async (req, res) => {
     await connectDB();
 
     // 최근 검색어 DB 조회
-    const searches = await RecentSearch.find({ user_snsId: snsId }).sort({
+    const searches = await RecentSearch.find({ user_snsId: snsId, is_delete: false }).sort({
       search_date: -1,
     });
 
@@ -154,14 +194,14 @@ export const saveRecentSearch = async (req, res) => {
 
     // 검색어 저장
     await RecentSearchText.findOneAndUpdate(
-      { user_snsId: snsId, search_text },
+      { user_snsId: snsId, is_delete: false, search_text },
       { $set: { search_date: getKoreanTime(), is_delete: false } },
       { upsert: true }
     );
 
     // searchStockList를 기반으로 검색어 존재 여부 확인 및 업데이트 또는 추가
     for (const stock of searchStockList) {
-      const existingSearch = await RecentSearch.findOne({ user_snsId: snsId, stock_name: stock });
+      const existingSearch = await RecentSearch.findOne({ user_snsId: snsId, is_delete: false, stock_name: stock });
 
       if (existingSearch) {
         // 존재하면 검색 날짜를 최신으로 업데이트
@@ -172,6 +212,7 @@ export const saveRecentSearch = async (req, res) => {
         const newSearch = new RecentSearch({
           user_id: user._id,
           user_snsId: snsId,
+          is_delete: false,
           stock_name: stock,
           search_date: getKoreanTime(),
         });
@@ -208,21 +249,52 @@ export const deleteRecentSearches = async (req, res) => {
     // 유저 쿠키 값을 가지고 user_id(인덱스) 값 가져오기
     const { snsId } = req.session;
 
-    await RecentSearch.deleteMany({ user_snsId: snsId }); // sns_id에 따른 검색한 주식 전부 삭제
-    await RecentSearchText.deleteMany({ user_snsId: snsId }); // sns_id에 따른 검색어 전부 삭제
+    // 유저 정보 조회
+    const user = await User.findOne({ sns_id: snsId, is_delete: false });
+    if (!user) {
+      res.status(401).json({ ok: false, message: "존재하지 않는 유저입니다." });
+      return;
+    }
+
+    await RecentSearch.deleteMany({ user_snsId: snsId, is_delete: false }); // sns_id에 따른 검색한 주식 전부 삭제
+    await RecentSearchText.deleteMany({ user_snsId: snsId, is_delete: false }); // sns_id에 따른 검색어 전부 삭제
     res.status(200).json({ ok: true, data: [], message: "recent searches deleted" });
   } catch (err) {
     res.status(500).json({ ok: false, message: err.message });
   }
 };
 
-export const deleteRecentSearchesItem = async (req, res) => {
+export const deleteRecentSearchItem = async (req, res) => {
   try {
-    const { snsId } = req.session;
+    const { search_text } = req.params;
+    // const { snsId } = req.session;
 
-    await RecentSearchText.deleteMany({ user_snsId: snsId }); // sns_id에 따른 검색어 전부 삭제
-    await RecentSearchText.delete;
-    res.status(200).json({ ok: true, data: [], message: "recent searches deleted" });
+    // // 유저 정보 조회
+    // const user = await User.findOne({ sns_id: snsId, is_delete: false });
+    // if (!user) {
+    //   res.status(401).json({ ok: false, message: "존재하지 않는 유저입니다." });
+    //   return;
+    // }
+
+    console.log(search_text);
+
+    const searchStockList = Object.entries(VARIOUS_STOCK_TO_NAME)
+      .filter((stock) => hangulIncludes(stock[0], search_text))
+      .map((stock) => stock[1]);
+
+    console.log(searchStockList);
+
+    // await RecentSearchText.deleteOne({ user_snsId: snsId, search_text, is_delete: false });
+    // await RecentSearch.deleteOne({ user_snsId: snsId, is_delete: false });
+
+    // const searchesText = await RecentSearchText.find(
+    //   { user_snsId: snsId, is_delete: false },
+    //   { _id: 0, search_text: 1, search_date: 1 }
+    // ).sort({
+    //   search_date: -1,
+    // });
+
+    res.status(200).json({ ok: true, data: [] });
   } catch (err) {
     res.status(500).json({ ok: false, message: err.message });
   }
---
+++ routes/search-route.js
@@ -0,0 +1,16 @@
+import express from "express";
+import * as searchController from "../controllers/search-controller.js";
+import { checkLoggedIn } from "../middleware/auth-middleware.js";
+
+const router = express.Router();
+
+router.get("/getPopularSearchesName", searchController.getPopularSearchesName); // 인기 검색어 가져오기 이름만
+router.get("/getPopularSearches", searchController.getPopularSearches); // 인기 검색어 가져오기 디테일 정보까지
+router.get("/getRecentSearches", checkLoggedIn, searchController.getRecentSearches); // 발견 페이지 - 최근 검색어 보여줄 때 사용
+router.get("/getSearchStocks/:search_text", searchController.getSearchStocks); // 발견 페이지 - 검색한 주식 조회
+router.get("/getRecentSearchDetails", checkLoggedIn, searchController.getRecentSearchDetails); // 관심 종목 페이지 모달 - 최근 검색한 종목 보여줄 때 사용
+router.patch("/saveRecentSearch", checkLoggedIn, searchController.saveRecentSearch); // 최근 검색어 저장
+router.delete("/deleteRecentSearches", checkLoggedIn, searchController.deleteRecentSearches); // 유저 인덱스 따른 최근 검색어 및 검색한 주식 전부 삭제
+router.delete("/deleteRecentSearchItem/:search_text", searchController.deleteRecentSearchItem); // 유저 인덱스 따른 최근 검색어 개별 삭제
+
+export default router;
