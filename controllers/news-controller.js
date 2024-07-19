db4cd660a4af830ab62c5fc92df416ff521f3429
윤태현 <yoonth0919@gmail.com>
2024-07-19 14:13:07 +0900

    hotfix: 유저 연동 실패 시 401, 값이 없을 경우 null이 아닌 빈 배열 반환

--- controllers/news-controller.js
+++ controllers/news-controller.js
@@ -45,7 +45,7 @@ export const getTodayPopularNews = async (req, res) => {
       startDate.setDate(startDate.getDate() - 1);
     }
 
-    res.status(200).json({ ok: true, data: news });
+    res.status(200).json({ ok: true, data: news ?? [] });
   } catch (err) {
     console.error(err);
     res.status(500).json({ ok: false, message: err.message });
@@ -74,7 +74,7 @@ export const getInterestStockNews = async (req, res) => {
       })
       .limit(6);
 
-    res.status(200).json({ ok: true, data: interestStockNews });
+    res.status(200).json({ ok: true, data: interestStockNews ?? [] });
   } catch (err) {
     console.error(err);
     res.status(500).json({ ok: false, message: err.message });
@@ -91,7 +91,7 @@ export const getRecentNews = async (req, res) => {
     });
     const recentNews = await News.find().sort({ published_time: -1 });
 
-    res.status(200).json({ ok: true, data: recentNews });
+    res.status(200).json({ ok: true, data: recentNews ?? [] });
   } catch (err) {
     console.error(err);
     res.status(500).json({ ok: false, message: err.message });
@@ -173,7 +173,7 @@ export const getSearchNews = async (req, res) => {
       view: -1,
     });
 
-    res.status(200).json({ ok: true, data: searchNews });
+    res.status(200).json({ ok: true, data: searchNews ?? [] });
   } catch (err) {
     console.error(err);
     res.status(500).json({ ok: false, message: err.message });
