6e3dce01c646b97278fe93faed4db4b58a7aeb3f
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-07-21 02:47:55 +0900

    refactor: 트랜잭션 처리로 인한 관심 주식 스키마 수정

--- schemas/interestStock-schema.js
+++ schemas/interestStock-schema.js
@@ -16,11 +16,8 @@ const InterestStockSchema = new mongoose.Schema({
 });
 
 // 한 개의 주식을 추가하는 메서드
-InterestStockSchema.methods.addStock = async function (stock) {
-  const maxOrder =
-    this.stock_list.length > 0
-      ? Math.max(...this.stock_list.map((s) => s.order))
-      : 0;
+InterestStockSchema.methods.addStock = async function (stock, options = {}) {
+  const maxOrder = this.stock_list.length > 0 ? Math.max(...this.stock_list.map((s) => s.order)) : 0;
 
   // 이미 추가된 주식인지 확인
   if (this.stock_list.find((s) => s.reuters_code === stock)) {
@@ -33,26 +30,23 @@ InterestStockSchema.methods.addStock = async function (stock) {
     order: maxOrder + 1,
   });
 
-  await this.save();
+  await this.save(options);
 };
 
 // 여러 개의 주식을 추가하는 메서드
-InterestStockSchema.methods.addStocks = async function (stocks) {
-  const currentMaxOrder =
-    this.stock_list.length > 0
-      ? Math.max(...this.stock_list.map((s) => s.order))
-      : 0;
+InterestStockSchema.methods.addStocks = async function (stocks, options = {}) {
+  const currentMaxOrder = this.stock_list.length > 0 ? Math.max(...this.stock_list.map((s) => s.order)) : 0;
   const stockEntries = stocks.map((stock, index) => ({
     reuters_code: stock,
     created_at: getKoreanTime(),
     order: currentMaxOrder + index + 1,
   }));
   this.stock_list = [...this.stock_list, ...stockEntries];
-  await this.save();
+  await this.save(options);
 };
 
 // 한 개의 주식을 삭제하는 메서드
-InterestStockSchema.methods.removeStock = async function (stock) {
+InterestStockSchema.methods.removeStock = async function (stock, options = {}) {
   this.stock_list = this.stock_list.filter((s) => s.reuters_code !== stock);
 
   // order 재정렬
@@ -60,31 +54,29 @@ InterestStockSchema.methods.removeStock = async function (stock) {
     s.order = index + 1;
   });
 
-  await this.save();
+  await this.save(options);
 };
 
 // 전체 주식 리스트를 업데이트하는 메서드
-InterestStockSchema.methods.updateStockList = async function (stocks) {
+InterestStockSchema.methods.updateStockList = async function (stocks, options = {}) {
   const stockEntries = stocks.map((stock, index) => ({
     reuters_code: stock,
     updated_at: getKoreanTime(),
     order: index + 1,
   }));
   this.stock_list = stockEntries;
-  await this.save();
+  await this.save(options);
 };
 
 // 주식 리스트 순서 업데이트 메서드
-InterestStockSchema.methods.updateStockOrder = async function (newOrder) {
+InterestStockSchema.methods.updateStockOrder = async function (newOrder, options = {}) {
   this.stock_list.forEach((s) => {
     s.order = newOrder.indexOf(s.reuters_code) + 1;
     s.updated_at = getKoreanTime();
   });
 
-  await this.save();
+  await this.save(options);
 };
 
-const InterestStock =
-  mongoose.models.InterestStock ||
-  mongoose.model("InterestStock", InterestStockSchema);
+const InterestStock = mongoose.models.InterestStock || mongoose.model("InterestStock", InterestStockSchema);
 export default InterestStock;
