7c7a53769f25e248f5ebffb4c579927d6c0adece
knoa0405 <knoa0405@gmail.com>
2024-07-30 16:46:52 +0900

    feat: update currency api

--- controllers/stock-controller.js
+++ controllers/stock-controller.js
@@ -1,3 +1,4 @@
+import CurrencyExchange from "../schemas/currency-exchange-schema.js";
 import Stock from "../schemas/stock-schema.js";
 
 // GET /stocks
@@ -6,6 +7,39 @@ export const getStocks = async (req, res) => {
     const stocks = await Stock.find();
 
     res.status(200).json({ ok: true, data: stocks });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ ok: false, message: error.message });
+  }
+};
+
+export const saveStockCurrency = async (req, res) => {
+  try {
+    const { nation, rate } = req.body;
+    const currencyExchange = new CurrencyExchange({
+      nation,
+      rate,
+    });
+
+    await currencyExchange.save();
+    res.status(201).json({ ok: true, message: "Currency exchange saved" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ ok: false, message: error.message });
+  }
+};
+
+export const getCurrencyExchanges = async (req, res) => {
+  try {
+    const currencyExchange = await CurrencyExchange.find({});
+
+    if (!currencyExchange) {
+      return res
+        .status(404)
+        .json({ ok: false, message: "Currency exchange not found" });
+    }
+
+    res.status(200).json({ ok: true, data: currencyExchange });
   } catch (error) {
     console.error(error);
     res.status(500).json({ ok: false, message: error.message });
